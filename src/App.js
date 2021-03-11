941c60e6fc3704ab4d6c7a1ecb7164f7316b6b3c
raulcattelan11@gmail.com <44942937+Rcc7@users.noreply.github.com>
2021-03-11 12:22:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import './components/Header/header'
+import Header from './components/Header/header';
+
+function App() {
+  return (
+    <div>     
+         <h2>Let's get started</h2>
+          <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+import logoUrl from '../../assets/logo.svg';
+
+const Header = () => (  
+<div className="header">
+        <p>fsfds</p>\
+        <img src={logoUrl} alt="Tesla" />  
+</div>
+)
+
+export default Header;
